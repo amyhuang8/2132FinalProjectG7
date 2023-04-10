83ecb3f9eef298cbd305517878c5a80d31a48e76
kkristene3 <kkristene3@gmail.com>
2023-04-10 11:31:50 -0400

    Add error handling if hotel number provided is not in the database

--- DatabaseProjectV3/src/main/java/com/example/ehotel/servlets/View2Servlet.java
+++ DatabaseProjectV3/src/main/java/com/example/ehotel/servlets/View2Servlet.java
@@ -27,20 +27,28 @@ public class View2Servlet extends HttpServlet {
         // READ THE FORM DATA
         int hotelID = Integer.parseInt(req.getParameter("hotel-id"));
 
-        LOGGER.info("Hotel ID: " + hotelID);
-
+        // set the session attribute
         req.setAttribute("hotel_id", hotelID);
 
         // get the data from view2
-        ArrayList<Room> capacityOfRooms = con.getView2(hotelID);
+        try {
+            ArrayList<Room> capacityOfRooms = con.getView2(hotelID);
+            //setting the attribute
+            req.setAttribute("capacityOfRooms", capacityOfRooms);
 
-        //setting the attribute
-        req.setAttribute("capacityOfRooms", capacityOfRooms);
-
-        // forward to the view2 page
-        req.setAttribute("capacityOfRooms", capacityOfRooms);
-        req.getRequestDispatcher("View2.jsp").forward(req, resp);
-        resp.sendRedirect("View2.jsp");
+            // forward to the view2 page
+            req.setAttribute("capacityOfRooms", capacityOfRooms);
+            req.getRequestDispatcher("View2.jsp").forward(req, resp);
+            resp.sendRedirect("View2.jsp");
+        } catch (Exception e) { // failure
+            //e.printStackTrace();
+            resp.setStatus(401); //setting error status
+            req.setAttribute("status", "CLOG-DB-401"); //setting error status attribute
+            req.setAttribute("heading", "INPUT ERROR"); //setting heading attribute
+            req.setAttribute("error_msg", "The hotel number provided is incorrect. Please return to the previous page, and try again."); //setting error msg attribute
+            req.getRequestDispatcher("ErrorPage.jsp").forward(req, resp); //forwarding response attributes to error page
+            resp.sendRedirect("ErrorPage.jsp"); //redirecting to error page
+        }
 
     }
 
